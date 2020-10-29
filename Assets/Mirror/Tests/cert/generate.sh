86b4f15f4e8ba254e4d5bbbbfe87ecf0c47f8b29
Paul Pacheco <paul.pacheco@aa.com>
2020-10-29 11:49:32 -0500

    Certificate generation script

--- Assets/Mirror/Tests/cert/generate.sh
+++ Assets/Mirror/Tests/cert/generate.sh
@@ -2,45 +2,75 @@
 
 # https://community.axway.com/s/question/0D52X000065Ykx2SAC/example-scripts-to-create-certificate-chain-with-openssl
 
-#Generate CA Certificate
+echo Generate CA Certificate
 
 #Generate private Key
 openssl genrsa -out CA.key 2048
 
 #Generate CA CSR
-openssl req -new -sha256 -key CA.key -out CA.csr -subj "/C=BR/ST=SAO PAULO/L=SAO PAULO/O=MirrorNG/CN=CA CERTIFICATE"
-
+openssl req -config openssl.cnf \
+    -key CA.key \
+    -new -sha256 -x509 -extensions v3_ca \
+    -subj "/C=US/ST=Texas/O=MirrorNG/CN=MIRRORNG CA" \
+    -out CA.pem 
+    
 #Generate CA Certificate (100 years)
-openssl x509 -signkey CA.key -in CA.csr -req -days 36500 -out CA.pem
 
 #--------------------------------------------------------------------------------------
 
-#Generate Intermediary CA Certificate
+echo Generate Intermediary CA Certificate
 
 #Generate private Key
 openssl genrsa -out CA_Intermediary.key 2048
 
 #Create Intermediary CA CSR
-openssl req -new -sha256 -key CA_Intermediary.key -out CA_Intermediary.csr -subj "/C=BR/ST=SAO PAULO/L=SAO PAULO/O=MirrorNG/CN=CA INTERMEDIARY CERTIFICATE"
+openssl req -config openssl.cnf \
+    -new -sha256 \
+    -subj "/C=US/ST=Texas/O=MirrorNG/CN=MIRRORNG INTERMEDIARY CA" \
+    -out CA_Intermediary.csr \
+    -key CA_Intermediary.key \
 
 #Generate Server Certificate (100 years)
-openssl x509 -req -in CA_Intermediary.csr -CA CA.pem -CAkey CA.key -CAcreateserial -out CA_Intermediary.crt -days 36500 -sha256
+#openssl x509 -req     -days 36500 -sha256
+
+openssl ca -config openssl.cnf \
+    -in CA_Intermediary.csr \
+    -out CA_Intermediary.crt \
+    -days 36500 \
+    -batch \
+    -extensions v3_intermediate_ca
 
 #--------------------------------------------------------------------------------------
 
-#Generate Server Certificate signed by Intermediary CA
+echo Generate Server Certificate signed by Intermediary CA
 
 #Generate private Key
-openssl genrsa -out ServerCert_signedByCAIntermediary.key 2048
+openssl genrsa -out localhost.key 2048
 
 #Ceate Server CSR
-openssl req -new -sha256 -key ServerCert_signedByCAIntermediary.key -out ServerCert_signedByCAIntermediary.csr -subj "/C=BR/ST=SAO PAULO/L=SAO PAULO/O=MirrorNG/CN=localhost/subjectAltName=DNS.1=axway.lab,DNS.2=your-alt-name"
+openssl req -config openssl.cnf \
+    -new -sha256 \
+    -key localhost.key \
+    -out localhost.csr \
+    -subj "/C=US/ST=Texas/O=MirrorNG/CN=localhost"
 
 #Generate Server Certificate
-openssl x509 -req -in ServerCert_signedByCAIntermediary.csr -CA CA.pem -CAkey CA.key -CAcreateserial -out ServerCert_signedByCAIntermediary.crt -days 36500 -sha256
+openssl ca -config openssl.cnf \
+    -extensions server_cert \
+    -days 36500 \
+    -notext \
+    -md sha256 \
+    -in localhost.csr \
+    -out localhost.crt \
+    -batch
+
+#openssl x509 -req -in ServerCert_signedByCAIntermediary.csr -CA CA_Intermediary.crt -CAkey CA_Intermediary.key -CAcreateserial -out ServerCert_signedByCAIntermediary.crt -days 36500 -sha256
 
 #View Certificate
-openssl x509 -text -noout -in ServerCert_signedByCAIntermediary.crt
+openssl x509 -text -noout -in localhost.crt
+
+#Generate chain
+cat localhost.crt CA_Intermediary.crt CA.pem > localhost_chain.crt
 
 #Package in a pfx
-openssl pkcs12 -export -out ServerCert_signedByCAIntermediary.pfx -inkey ServerCert_signedByCAIntermediary.key -in ServerCert_signedByCAIntermediary.crt -passout "pass:password" -certfile CA_Intermediary.crt
+openssl pkcs12 -export -out localhost.pfx -inkey localhost.key -in localhost.crt -certfile CA_Intermediary.crt -passout "pass:password"
